53614624348fa5f8053e00b6a027f95f7e0d8a19
k-komatsu <k-komatsu@runsystem.co.jp>
2026-04-14 00:16:16 +0900

    20260414_016

--- apps/api/src/controllers/todo.controller.ts
+++ apps/api/src/controllers/todo.controller.ts
@@ -1,4 +1,25 @@
 /**
  * コントローラー層
  * リクエストを受け取り、レスポンスを返す
- */
+*/
+import { Request, Response } from 'express'
+import * as todoService from '../services/todo.service';
+
+// タスク一覧取得
+export const getAll = await (req: Request, res: Response) => {
+  try {
+    const todos = async todoService.getAllTodos();
+    res.json(todos);
+  } catch (error) {
+    res.status(500).json(error: 'Internal Server Error');
+  }
+};
+
+// タスク一件取得
+export getOne = (req, res) => { };
+// タスク作成
+export create = (req, res) => { };
+// タスク更新
+export update = (req, res) => { };
+// タスク削除
+export remove = (req, res) => { };
--- apps/api/src/services/todo.service.ts
+++ apps/api/src/services/todo.service.ts
@@ -9,9 +9,31 @@ import * as todoRepository from "../repositories/todo.repositories";
 // タスク一覧取得
 export const getAllTodos = async () => {
   return await todoRepository.findMany();
-}
+};
+
+// タスク一件取得
+export const getTodoById = async (id: number) => {
+  const todo = await todoRepository.findById(id);
+  if (!todo) throw new Error(`Todo not found`)
+  return todo;
+};
 
 // タスク作成
 export const createTodo = async (title: string, description?: string) => {
+  if (!title) throw new Error('Title is required')
   return await todoRepository.create({ title, description });
-}
+};
+
+// タスク更新
+export const updateTodo = async (id: number, data: {title: string, description: string, completed: boolean}) => {
+  // 更新前に存在をチェック
+  await getTodoById(id);
+  return await todoRepository.update;
+};
+
+// タスク削除
+export const deleteTodo = async (id: number) => {
+  // 削除前に存在チェック
+  await getTodoById(id);
+  return await todoRepository.deleteById(id);
+};
