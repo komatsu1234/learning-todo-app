72a0f17535dbf4840cd7edcccd993bdf53862189
k-komatsu <k-komatsu@runsystem.co.jp>
2026-04-22 00:12:12 +0900

    20260422_012

--- apps/api/src/validations/todo.validation.ts
+++ apps/api/src/validations/todo.validation.ts
@@ -1,22 +1,34 @@
 import { z } from `zod`;
 
-// タスク作成
+/**
+ * タスク作成
+ * trim(): 空白文字を防ぐ
+ * optional(): Null許容
+ * */
 export const createTodoSchema = z.object({
-  title: z.string().min(1, "タイトルは必須です").max(100, "100文字以内で入力してください。"),
-  description: z.string().max(500, "500文字以内で入力してください。"),
+  title: z.string().trim().min(1, "タイトルは必須です").max(100, "100文字以内で入力してください"),
+  description: z.string().optional().max(500, "500文字で入力してください。"),
 });
-
 
 /**
  * ID（パスパラメータ）のバリデーション
- * corers(): 
+ * corers(): 文字列を数値に変換
+ * int(): 整数かどうか
+ * positive(): 正の数かどうか　「0」を含む場合 `nonnegative()` を利用する
  *  */
 export const idSchema = z.object({
-  prams: z.object({
-    id: z.corers().number().int().positive(),
+  params: z.object({
+    id: z.coerce.number().int().positive()
   }),
-});
+})
 
+/**
+ * タスク更新
+ * 
+ */
+export const updateTodoSchema = z.object({
+  title: z.string().trim().min(1).optional(),
+}).refine(v => Object.keys(v).length > 0, { message: '更新項目が必要です' });
 
 // 型をエクスポートし再利用できる
 export type CreateTodoInput = z.infer<typeof createTodoSchema>['body'];
